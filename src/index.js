07bf88689972f307ebc23aa2117acf293c445987
KiranmaiPasupuleti <pasupuletikiranmai1@gmail.com>
2022-12-12 10:47:38 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//https://krpmapp.ccbp.tech
+
+https: import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
